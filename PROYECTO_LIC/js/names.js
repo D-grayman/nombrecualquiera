a79bc8a665910a649b98277da4809404b66ace23
William-Valdez <willvaldez@outlook.es>
2014-11-04 12:32:03 -0600

    Proyecto de LIC

---
+++ PROYECTO_LIC/js/names.js
@@ -0,0 +1,190 @@
+var nombres = {
+"Personas": [
+{"imagen" : "presidentes/tomas_regalado.jpg",
+"nombre" : "Tomás",
+"apellido" : "Regalado",
+"mandato" : "14 de noviembre de 1898 <br>- 1 de marzo de 1903"},
+{"imagen" : "presidentes/pedroescalon.jpg",
+"nombre" : "Pedro José ",
+"apellido" : "Escalon",
+"mandato" : "1 de marzo de 1903 <br>- 1 de marzo de 1907"},
+
+{"imagen" : "presidentes/Manuel_Enrique_Araujo.jpg",
+"nombre" : "Manuel Enrique",
+"apellido" : "Araujo",
+"mandato" : "1 de marzo de 1911 <br>- 8 de febrero de 1913"},
+
+{"imagen" : "presidentes/Alfonso_Quiñónez_Molina.jpg",
+"nombre" : "Alfonso",
+"apellido" : "Quiñorez",
+"mandato" : "<B>(PROV.:09/02/1913-29/08/1914)</B><BR> 1 de marzo de 1915 <BR>- 21 de diciembre de 1918"},
+
+{"imagen" : "presidentes/Jorge_Melendez.jpg",
+"nombre" : "Jorge",
+"apellido" : "Meléndez",
+"mandato" : "1 de marzo 1919 <br>- 1 de marzo de 1923"},
+
+{"imagen" : "presidentes/Pio.jpg",
+"nombre" : "<br>Pio",
+"apellido" : "Romero Bosque",
+"mandato" : "1 de marzo de 1927 <br>- 1 de marzo de 1931"},
+
+{"imagen" : "presidentes/Arturo_Araujo.jpg",
+"nombre" : "Arturo",
+"apellido" : "Araujo",
+"mandato" : "1 de marzo - 2 de diciembre de 1931"},
+
+
+{"imagen" : "presidentes/Directorio_Civico.jpg",
+"nombre" : "<br><br><br><br>Direcorio",
+"apellido" : "Civico de 1931",
+"mandato" : "2- 4 de diciembre de 1931"},
+
+
+{"imagen" : "presidentes/maximiliano.jpg",
+"nombre" : "<br>Maximiliano",
+"apellido" : "Hernandez Martinez",
+"mandato" : "4/12/1931 - 28/8/1934 <br><br>y 1/03/1935 - 9/05/1944"},
+
+{"imagen" : "presidentes/Andres_Ignacio.jpg",
+"nombre" : "Andrés Ignacio",
+"apellido" : "Hernandez",
+"mandato" : "<b>Provisional: <br>29/08/1934 - 1/03/1935</b><br><br>9/05 - 20/10/1944"},
+
+{"imagen" : "presidentes/Osmin.jpg",
+"nombre" : "Osmin Aguirre",
+"apellido" : "y Salinas",
+"mandato" : "21 de octubre de 1944 <br>- 1 de marzo de 1945"},
+
+
+{"imagen" : "presidentes/Castaneda.jpg",
+"nombre" : "Salvador",
+"apellido" : "castaneda Castro",
+"mandato" : "1 de marzo de 1945 <br>- 14 de diciembre de 1948"},
+
+
+{"imagen" : "presidentes/Consejo_Revolucionario.jpg",
+"nombre" : "Consejo Revolucionario",
+"apellido" : "de Gobierno",
+"mandato" : "15 de diciembre de 1948 <br>- 14 de septiembre de 1950"},
+
+
+{"imagen" : "presidentes/oscarosorio.jpg",
+"nombre" : "Oscar",
+"apellido" : "osorio",
+"mandato" : "14 de septiembre de 1950 <br>- 14 de septiembre de 1956"},
+
+
+{"imagen" : "presidentes/josemaria.jpg",
+"nombre" : "José Maria",
+"apellido" : "Lemus",
+"mandato" : "14 de septiembre de 1956 <br>- 26 de octubre de 1960"},
+
+
+{"imagen" : "presidentes/junta_gobierno.jpg",
+"nombre" : "Junta",
+"apellido" : "de Gobierno",
+"mandato" : "26 de octubre de 1960 <br>- 25 de enero de 1961"},
+
+
+{"imagen" : "presidentes/directoriocivicomilitar1961.jpg",
+"nombre" : "Directorio Cívico",
+"apellido" : "<br>Militar",
+"mandato" : "25 de enero de 1961 <br>- 25 de enero de 1962"},
+
+
+{"imagen" : "presidentes/cea.jpg",
+"nombre" : "Eusebio Rodolfo",
+"apellido" : "Cea",
+"mandato" : "25 de enero <br>- 1 de julio de 1962"},
+
+
+{"imagen" : "presidentes/Julio_Adalberto_Rivera.jpg",
+"nombre" : "Julio Adalberto",
+"apellido" : "Rivera",
+"mandato" : "1 de julio de 1962 <br>- 1 de julio de 1967"},
+
+
+{"imagen" : "presidentes/fidel.jpg",
+"nombre" : "Fidel",
+"apellido" : "Sanchez Hernández",
+"mandato" : "1 de julio de 1967 <br>- 1 de julio de 1972"},
+
+
+{"imagen" : "presidentes/arturo_molina.jpg",
+"nombre" : "Arturo Armando",
+"apellido" : "Molina",
+"mandato" : " de julio de 1972 <br>- 1 de julio de 1977"},
+
+{"imagen" : "presidentes/humberto.jpg",
+"nombre" : "Carlos Humberto",
+"apellido" : "Romero",
+"mandato" : "1 de julio de 1977 <br>- 15 de octubre de 1979"},
+
+{"imagen" : "presidentes/Consejo_Revolucionario.jpg",
+"nombre" : "Junta Revolucionaria",
+"apellido" : "de Gobierno",
+"mandato" : "15 de octubre de 1979 <br>- 2 de mayo de 1982"},
+
+{"imagen" : "presidentes/Alvaro.jpg",
+"nombre":"Alvaro",
+"apellido" : "Magaña",
+"mandato" : "2 de mayo de 1982 <br>- 1 de junio de 1984"},
+
+{"imagen" : "presidentes/napoleon.jpg",
+"nombre" : "José Napoleón",
+"apellido" : "Duarte",
+"mandato" : "1 de junio de 1984 <br>- 1 de junio de 1989"},
+
+{"imagen" : "presidentes/AlfredoCristiani.jpg",
+"nombre" : "Alfredo",
+"apellido" : "Cristiani",
+"mandato" : "1 de junio de 1989 <br>- 1 de junio de 1994"},
+
+{"imagen" : "presidentes/calderon_sol.jpg",
+"nombre" : "Armando",
+"apellido" : "Calderón Sol",
+"mandato" : "1 de junio de 1994 <br>- 1 de junio de 1999"},
+
+{"imagen" : "presidentes/paco_flores.jpg",
+"nombre" : "Francisco",
+"apellido" : "Flores Perez",
+"mandato" : "1 de junio de 1999 <br>- 1 de junio de 2004"},
+
+{"imagen" : "presidentes/Elias-Antonio-Saca-Gonzalez.jpg",
+"nombre" : "Elías António",
+"apellido" : "Saca González",
+"mandato" : "1 de junio de 2004 <br>- 1 de junio de 2009"},
+
+{"imagen" : "presidentes/funes.jpg",
+"nombre" : "Mauricio",
+"apellido" : "Funes cartagena",
+"mandato" : "1 de junio de 2009 <br>- 1 de junio de 2014"},
+
+{
+ "imagen": "presidentes/salvador-sanchez-ceren.jpg",
+ "nombre":  "Salvador",
+ "apellido":    "sanchez Ceren",
+ "mandato": "1 de junio de 2014 <br>- <b>Presente</b>"},
+]
+};
+//Obteniendo el elemento contenedor donde se cargarán
+//todos los datos del objeto JSON
+var div = document.getElementById("info");
+div.innerHTML = volcarDatos(nombres.Personas);
+function volcarDatos(datos){
+var total = datos.length;
+data = "<ul class=\"grid\">\n";
+for(var i=0; i<total; i++){
+data += "<li class=\"box\">\n";
+data += "<img src=\"" + datos[i].imagen + "\" alt=\"Avatar de " +
+datos[i].nombre + " " + datos[i].apellido + "\" class=\"avatar\" />\n";
+data += "<h4> " + datos[i].nombre + " " + datos[i].apellido +
+"\n</h4>\n";
+data += "<br><p><b>\nPeriodo:</b><br> " + datos[i].mandato + "\n<br />\n";
+data += "</div>\n";
+data += "</li>\n";
+}
+data += "</ul>\n";
+return data;
+}
